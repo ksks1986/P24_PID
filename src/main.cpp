76ef297231a0d3f30ca1400906588bb1349f19e4
ksks1986 <32826518+ksks1986@users.noreply.github.com>
2018-06-16 04:39:39 +0900

    re-submit

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  double Kp = 0.5;
-  double Ki = 0.01;
-  double Kd = 50;
+  double Kp = 0.4;
+  double Ki = 0.0005;
+  double Kd = 3.5;
   pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
